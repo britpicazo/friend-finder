e2060de1df183127cb6955f6aea681e860ab7261
Brittany Picazo <britpicazo@gmail.com>
2017-06-02 15:54:01 -0500

    htmlRoutes, friends.js done, added package.json

---
+++ app/data/friends.js
@@ -0,0 +1,72 @@
+var friendsArray = [
+    {
+        "name": "Alison",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            5, 1, 4, 4, 5, 1, 2, 5, 4, 1
+        ]
+    },
+    {
+        "name": "Brittany",
+        "photo": "",
+        "scores": [
+            3, 4, 4, 4, 2, 4, 3, 4, 2, 2
+        ]
+    },
+    {
+        "name": "Caleb",
+        "photo": "",
+        "scores": [
+            5, 4, 3, 2, 1, 2, 3, 4, 5, 5
+        ]
+    },
+    {
+        "name": "Deanna",
+        "photo": "",
+        "scores": [
+            2, 2, 3, 3, 4, 4, 5, 5, 1, 1
+        ]
+    },
+    {
+        "name": "Eve",
+        "photo": "",
+        "scores": [
+            1, 1, 2, 2, 3, 3, 4, 4, 5, 5
+        ]
+    },
+    {
+        "name": "Fred",
+        "photo": "",
+        "scores": [
+            3, 3, 4, 4, 5, 5, 1, 1, 2, 2
+        ]
+    },
+    {
+        "name": "George",
+        "photo": "",
+        "scores": [
+            4, 4, 5, 5, 1, 1, 2, 2, 3, 3
+        ]
+    },
+    {
+        "name": "Harry",
+        "photo": "",
+        "scores": [
+            3, 4, 1, 5, 3, 2, 1, 1, 4, 5
+        ]
+    },
+    {
+        "name": "Ingrid",
+        "photo": "",
+        "scores": [
+            2, 5, 1, 2, 4, 3, 3, 1, 4, 5
+        ]
+    },
+    {
+        "name": "Jeremy",
+        "photo": "",
+        "scores": [
+            1, 4, 2, 5, 2, 3, 3, 4, 1, 5
+        ]
+    }
+]
---
+++ server.js
@@ -0,0 +1,11 @@
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// BodyParser makes it possible for our server to interpret data sent to it.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
